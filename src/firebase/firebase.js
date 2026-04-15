2c7266f64cdbbd8e5b99b73c0ccd7d3a93fcec76
josephgakono <sirg4kono@gmail.com>
2026-04-15 13:27:17 +0300

    change firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,15 +8,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDAH6iRo-N2MWpH9cPaDPdOxTBTnRv2Me4",
-  authDomain: "kibanda-b7136.firebaseapp.com",
-  projectId: "kibanda-b7136",
-  storageBucket: "kibanda-b7136.firebasestorage.app",
-  messagingSenderId: "692613480817",
-  appId: "1:692613480817:web:d2bfc29617c914702936d0",
-  measurementId: "G-F34B151YDH"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
